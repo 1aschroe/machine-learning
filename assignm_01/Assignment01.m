77d79e39e930755855fffbe63013022a9a1a2779
Arne Schröder <Arne.Schroeder@informatik.uni-hamburg.de>
2013-04-08 13:52:20 +0200

    Sektion 3, Tippfehler in 1, ASV-Dateien werden ignoriert

--- assignm_01/Assignment01.m
+++ assignm_01/Assignment01.m
@@ -6,9 +6,18 @@ n2 = 30;
 train_data_class2 = rand(n2,2) + ones(n2,2)*[1 0; 0 0];
 train_data = [train_data_class1; train_data_class2];
 train_label(1:n1) = 1;
-train_label(n1+1:n2) = 2;
+train_label(n1+1:n1+n2) = 2;
 
 % Section 2
 figure(1); clf; hold all; axis equal;
 plot(train_data(1:n1,1), train_data(1:n1,2), 'r*');
 plot(train_data(n1+1:n1+n2,1), train_data(n1+1:n1+n2,2), 'bo');
+
+% Section 3
+n1 = 100;
+test_data_class1 = rand(n1,2);
+n2 = 100;
+test_data_class2 = rand(n2,2) + ones(n2,2)*[1 0; 0 0];
+test_data = [test_data_class1; test_data_class2];
+test_label(1:n1) = 1;
+test_label(n1+1:n1+n2) = 2;
