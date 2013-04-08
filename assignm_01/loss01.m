61dd0c1743c91112fdea62f7edbe367a8209e49b
Angel Bakardzhiev <angel.bakardzhiev@informatik.uni-hamburg.de>
2013-04-08 15:18:54 +0200

    Added new function loss01 (not complete yet).

---
+++ assignm_01/loss01.m
@@ -0,0 +1,9 @@
+function err = loss01(y_pred, y);
+% LOSS01 Gets as input a prediction calculated by the knnClasifier and
+% correct labels y. The function returns the average error (empirical risk
+% with respect to the 0-1 loss) for this prediction.
+
+for it = 1:lenght(y)
+end
+
+end
