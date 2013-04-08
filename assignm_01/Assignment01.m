f64dc1066190f5506926dff3ccec4ed4e1209416
Angel Bakardzhiev <angel.bakardzhiev@informatik.uni-hamburg.de>
2013-04-08 14:24:36 +0200

    Added description of the data to Section 1.

--- assignm_01/Assignment01.m
+++ assignm_01/Assignment01.m
@@ -1,5 +1,10 @@
 
-% Section 1
+%% Section 1
+% The data for class 1 is random 30 x 2 matrix with values in the interval (0.0, 1.0).
+% The data for class 2 is again random 30 x 2 matrix, but the first column has
+% values in the interval (1.0, 2.0).
+% The data is represented as 2D points when ploted.
+
 n1 = 30;
 train_data_class1 = rand(n1,2);
 n2 = 30;
@@ -8,12 +13,12 @@ train_data = [train_data_class1; train_data_class2];
 train_label(1:n1) = 1;
 train_label(n1+1:n1+n2) = 2;
 
-% Section 2
+%% Section 2
 figure(1); clf; hold all; axis equal;
 plot(train_data(1:n1,1), train_data(1:n1,2), 'r*');
 plot(train_data(n1+1:n1+n2,1), train_data(n1+1:n1+n2,2), 'bo');
 
-% Section 3
+%% Section 3
 n1 = 100;
 test_data_class1 = rand(n1,2);
 n2 = 100;
