e115f9b4e05e4756cc2e95b0288f0a9d12f52f8e
Angel Bakardzhiev <angel.bakardzhiev@informatik.uni-hamburg.de>
2013-04-08 13:24:45 +0200

    Added training data.

--- assignm_01/Assignment01.m
+++ assignm_01/Assignment01.m
@@ -1,1 +1,7 @@
-a = 2;
+n1 = 30;
+train_data_class1 = rand(n1,2);
+n2 = 30;
+train_data_class2 = rand(n,2) + ones(n2,2)*[1 0; 0 0];
+train_data = [train_data_class1; train_dta_class2];
+train_label(1:n1) = 1;
+train_label(n1+1:n2) = 2;
