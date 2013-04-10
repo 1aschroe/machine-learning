29ea2c4e57e3e25f54a94b0bda31abe5dc5fc044
Arne Schröder <Arne.Schroeder@gmail.com>
2013-04-10 13:17:34 +0200

    gemeinsamen Teil von Aufgabe 1 und 2 in eigene Funktion refaktoriert

--- assignm_01/doExercise1.m
+++ assignm_01/doExercise1.m
@@ -31,32 +31,14 @@ test_label(1:n1,1) = 1;
 test_label(n1+1:n1+n2,1) = 2;
 
 %% Section 4
-%% See function knnClassify
+% See function knnClassify
 
 %% Section 5
-%% see function loss01
+% see function loss01
 
 %% Section 6
-k_values = [1 3 5 7 9 10 11 15 19 20];
-error_test = zeros(1, length(k_values));
-error_train = zeros(1, length(k_values));
-for it = 1:length(k_values)
-    test_pred_label = knnClassify(train_data, train_label, test_data, k_values(it));
-    error_test(it) = loss01(test_pred_label, test_label);
-    train_pred_label = knnClassify(train_data, train_label, train_data, k_values(it));
-    error_train(it) = loss01(train_pred_label, train_label);
-end
+[minK, errValue] = evaluateK(train_data, train_label, test_data, test_label);
 
-%% Section 7
-
-figure(2); clf; hold all;
-plot(k_values,error_test,'r*:');
-plot(k_values,error_train,'b.-');
-
-
-%% Section 8
-[sorted, index] = sort(error_test);
-minK = k_values(index(1));
 pred_label = knnClassify(train_data, train_label, test_data, minK);
 
 figure(3); clf; hold all; axis equal;
@@ -66,7 +48,6 @@ plot(test_data(pred_class1,1),test_data(pred_class1,2),'r*');
 plot(test_data(pred_class2,1),test_data(pred_class2,2),'bo');
 plot([1 1],[0 1],'k');
 text(1,-0.1,['min K = ', int2str(minK)]);
-errValue=sorted(1)
 text(1,-0.2,['err Value = ', num2str(errValue,5)]);
 
 end
--- assignm_01/doExercise2.m
+++ assignm_01/doExercise2.m
@@ -19,26 +19,6 @@ imagesc(dig)
 colormap('gray');
 
 %% Section 4
-k_values = [1 3 5 7 9 10 11 15];
-error_test = zeros(1, length(k_values));
-error_train = zeros(1, length(k_values));
-for it = 1:length(k_values)
-    disp(['Working on test data for k=' int2str(k_values(it))]);
-    tic
-    test_pred_label = knnClassify(x_train, y_train, x_test, k_values(it));
-    error_test(it) = loss01(test_pred_label, y_test);
-    toc
-    
-    disp(['Working on training data for k=' int2str(k_values(it))]);
-    tic
-    train_pred_label = knnClassify(x_train, y_train, x_train, k_values(it));
-    error_train(it) = loss01(train_pred_label, y_train);
-    toc
-end
-
-% Plotting
-figure(5); clf; hold all;
-plot(k_values(1:it),error_test(1:it),'r*:');
-plot(k_values(1:it),error_train(1:it),'b.-');
+evaluateK(x_train, y_train, x_test, y_test);
 end
 
