cce7cbb5cebc49ffa5e94e043796f31f6fc78330
foswald <falk.oswald@gmail.com>
2013-04-10 12:07:07 +0200

    Trying to display results during computation.

--- assignm_01/Assignment01.m
+++ assignm_01/Assignment01.m
@@ -28,6 +28,7 @@ x_test = double(test_data(testList,:));
 y_test = double(test_label(testList));
 
 %% Section 3
+figure(4);
 dig = reshape(train_data(1012,:),16,16);
 imagesc(dig)
 colormap('gray');
@@ -36,13 +37,17 @@ colormap('gray');
 k_values = [1 3 5 7 9 10 11 15];
 error_test = zeros(1, length(k_values));
 error_train = zeros(1, length(k_values));
+figure(5); clf; hold all;
 for it = 1:length(k_values)
+    disp(['Working on test data for k=' int2str(k_values(it))]);
     test_pred_label = knnClassify(x_train, y_train, x_test, k_values(it));
     error_test(it) = loss01(test_pred_label, y_test);
+    
+    plot(k_values(1:it),error_test(1:it),'r*:');
+    
+    disp(['Working on training data for k=' int2str(k_values(it))]);
     train_pred_label = knnClassify(x_train, y_train, x_train, k_values(it));
     error_train(it) = loss01(train_pred_label, y_train);
+    
+    plot(k_values(1:it),error_train(1:it),'b.-');
 end
-
-figure(5); clf; hold all;
-plot(k_values,error_test,'r*:');
-plot(k_values,error_train,'b.-');
