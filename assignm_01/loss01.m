a2c54f6cb898ff2abe0487b99b742795638789d9
Angel Bakardzhiev <angel.bakardzhiev@informatik.uni-hamburg.de>
2013-04-14 23:44:06 +0200

    A01: Added latex document.

--- assignm_01/loss01.m
+++ assignm_01/loss01.m
@@ -1,5 +1,5 @@
 function err = loss01(y_pred, y)
-% LOSS01 Gets as input a prediction calculated by the knnClasifier and
+% LOSS01 Gets as input a prediction calculated by the knnClasifiy and
 % correct labels y. The function returns the average error (empirical risk
 % with respect to the 0-1 loss) for this prediction.
 
