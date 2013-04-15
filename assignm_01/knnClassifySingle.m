b819f820fd80235ad1593b93478c60ec9649d73d
Arne Schröder <Arne.Schroeder@gmail.com>
2013-04-15 11:23:34 +0200

    Musterlösung

--- assignm_01/knnClassifySingle.m
+++ assignm_01/knnClassifySingle.m
@@ -3,8 +3,10 @@ function pred = knnClassifySingle( train_data, train_label, test_datum, k )
 %datum
 % assumes labels to be either 1 or 0
 
-distances = sqrt(sum((train_data - ones(size(train_data,1),1)*test_datum).^2,2));
-[sorted,index] = sort(distances);
+%Unserse Lösung
+%distances = sqrt(sum((train_data - ones(size(train_data,1),1)*test_datum).^2,2));
+distances = pdist2(test_data, train_data, 'euclidean');
+[~,index] = sort(distances);
 closest_labels = train_label(index);
 
 pred = mode(closest_labels(1:k));
--- assignm_01/loss01.m
+++ assignm_01/loss01.m
@@ -5,7 +5,9 @@ function err = loss01(y_pred, y)
 
 % assuming that y and y_pred are either 1 or 2
 
-number_errors = sum(mod(y_pred+y,2));
+%number_errors = sum(mod(y_pred+y,2));
+%number_errors=sum(ne(y_pred, y)); %problem equal = 1 not 0..., so i used ne(not equal)
+number_errors = length(find(y_pred~=y));
 err = number_errors / length(y);
 
 end
