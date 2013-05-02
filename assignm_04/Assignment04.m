7bbdf3bc10a8a2c087840d0e357dfdaab201557b
foswald <falk.oswald@gmail.com>
2013-05-02 20:47:54 +0200

    Hab kommentare ergänzt und variablen umbenannt (damit ichs auch verstehe ;). Außerdem hab ich one_vs_one überarbeitet: Die 3D matrix fand ich unschön und unübersichtlich (außerdem war sie halb leer). Die schleife ist nun auch deutlich kürzer.

--- assignm_04/Assignment04.m
+++ assignm_04/Assignment04.m
@@ -3,83 +3,102 @@
 load('usps_train.mat');
 load('usps_test.mat');
 %% Section 1
-trList = find(train_label==4 | train_label==9);
-x_train = double(train_data(trList,:));
-y_train = double(train_label(trList));
+% find indexes of labels 4 & 9
+trainList = find(train_label==4 | train_label==9);
+% load train data of indexes
+% x_train contains grayscale vectors (16x16 bit)
+x_train = double(train_data(trainList,:));
+% y_train the labels to the vectors
+y_train = double(train_label(trainList));
+
 
 testList = find(test_label==4 | test_label==9);
+% load test data of indexes
+% x_test contains grayscale vectors (16x16 bit)
 x_test = double(test_data(testList,:));
+% y_test the labels to the vectors
 y_test = double(test_label(testList));
 % end of copy
 
+% cls is a discriminant analysis classifier based on the input variables
 cls = ClassificationDiscriminant.fit(x_train, y_train);
 
 % display of the linear coefficients for predicting the class
+% positive values are more likely to be class 1 (4) and negative values are
+% more likely to be class 2 (9)
 dig = reshape(cls.Coeffs(1,2).Linear,16,16);
-imagesc(dig)
+imagesc(dig);
 
 %% Section 2
 
 % positive values indicate class 1 (4) negative class 2 (9), we take the
 % sign to predict the class
-pred_test = sign(evaluateCls(cls, 1, 2, x_test));
+pred_labels = sign(evaluateCls(cls, 1, 2, x_test));
 
-% the real y-values are converted to -1 and 1 and compared to our
-% predictions
-loss01(pred_test, (y_test==4)-(y_test==9))
+% the real y-values are converted to -1 and 1 (all entries except 4 and 9 are zero,
+% so (y_test==4) is 1 where 4 is the value and '-(y_test==9)' is -1 where 9
+% is the value.
+ref_labels = (y_test==4)-(y_test==9);
+% calculate the loss
+err_4_9 = loss01(pred_labels, ref_labels)
 
 %% Exercise 2
-
-trList = find(train_label==4 | train_label==3 | train_label==2 | train_label==1);
-x_train = double(train_data(trList,:));
-y_train = double(train_label(trList));
+% Load all data for Exercise 2
+trainList = find(train_label==4 | train_label==3 | train_label==2 | train_label==1);
+x_train = double(train_data(trainList,:));
+y_train = double(train_label(trainList));
 
 testList = find(test_label==4 | test_label==3 | test_label==2 | test_label==1);
 x_test = double(test_data(testList,:));
 y_test = double(test_label(testList));
 
 %% one vs. all
+% Each classifier gets trained for one class, so cls1 classifies for 1 and !1, cls2
+% for !2 and so on.
 cls1 = ClassificationDiscriminant.fit(x_train, y_train==1);
 cls2 = ClassificationDiscriminant.fit(x_train, y_train==2);
 cls3 = ClassificationDiscriminant.fit(x_train, y_train==3);
 cls4 = ClassificationDiscriminant.fit(x_train, y_train==4);
 
+% All weights for the testdata are collected from each classifier.
+% The data is written in a 200x4 matrix.
 weights = zeros(length(x_test), 4);
 weights(:,1) = evaluateCls(cls1, 1, 2, x_test);
 weights(:,2) = evaluateCls(cls2, 1, 2, x_test);
 weights(:,3) = evaluateCls(cls3, 1, 2, x_test);
 weights(:,4) = evaluateCls(cls4, 1, 2, x_test);
 
-[~,pred] = min(weights,[], 2);
+% min returns the smalles row entry of the weight matrix
+% pred_labels holds the indexes of the columns which are fortunately our labels
+[~,pred_labels] = min(weights,[], 2);
 
-% the real y-values are converted to -1 and 1 and compared to our
-% predictions
-loss01(pred, y_test)
+% We compare the predicted labels with the reference labels to generate our
+% loss
+one_vs_all = loss01(pred_labels, y_test)
 
 %% one vs. one
-
+% for one vs. one we train our classifier with all labels (i.e. it has four
+% classes to distinguish)
 clsX = ClassificationDiscriminant.fit(x_train, y_train);
 
-weights = zeros(3, 3, length(x_test));
+% Our weights are stored in a 400x6 matrix, where each column represents a 
+% unique pair of classes:
+classRef = [1 2; 1 3; 1 4; 2 3; 2 4; 3 4];
+weights = zeros(length(x_test), 6);
 
-for a=1:3
-    for b=(a+1):4
-        weights(a, b - 1, :) = evaluateCls(clsX, a, b, x_test);
-    end
+% iterate over class combinations and generate weights
+for it=1:length(classRef)
+    classA = classRef(it, 1);
+    classB = classRef(it, 2);
+    weights(:,it) = evaluateCls(clsX, classA, classB, x_test);
 end
 
-pred = zeros(length(x_test), 1);
-
+pred_labels = zeros(length(x_test), 1);
+% for each row find the maximum absolute value and put it into the
+% predtiction data
 for it=1:length(x_test)
-    slice = abs(weights(:,:,it));
-    [~, index] = max(slice(:));
-    index2 = floor((index - 1) / 3) + 1;
-    index1 = mod((index - 1), 3) + 1;
-    if weights(index1, index2, it)>0
-        pred(it) = index1;
-    else
-        pred(it) = index2 + 1;
-    end
+    [val_pos, max_pos] = max(abs(weights(it, :)));
+    pred_labels(it) = max_pos;
 end
 
-loss01(pred, y_test)
+one_vs_one = loss01(pred_labels, y_test)
--- assignm_04/evaluateCls.m
+++ assignm_04/evaluateCls.m
@@ -1,7 +1,12 @@
 function [ pred ] = evaluateCls( cls, class1, class2, x_test )
-%EVALUATECLS Summary of this function goes here
-%   Detailed explanation goes here
+%EVALUATECLS Takes a classifier and tests x_tests against the two annotated
+%classes.
+%Returns pred which is a vector containing the predicted class for each
+%x_test: positive values belong to class1, negative to class2. Higher
+%values indicate more confidence on the prediction.
+const = cls.Coeffs(class1,class2).Const;
+linear = cls.Coeffs(class1,class2).Linear;
 
-pred = cls.Coeffs(class1,class2).Const*ones(size(x_test,1),1) + x_test*cls.Coeffs(class1,class2).Linear;
+pred = const * ones(size(x_test,1),1) + x_test * linear;
 
 end
