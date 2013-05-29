fc8c3b8f235d6cd90692f872f5ed682633846f2c
foswald <falk.oswald@gmail.com>
2013-05-29 10:40:06 +0200

    kommentare und variablen umbenennung

--- assignm_07/assignment07.m
+++ assignm_07/assignment07.m
@@ -1,15 +1,21 @@
 
+%%Load shuttle data
+
 % using StatLog Shuttle data:
 % http://archive.ics.uci.edu/ml/datasets/Statlog+%28Shuttle%29
 load('shuttle.trn');
+% Shuttle dataset has 9 entries
 x=shuttle(:,1:9);
+% tenth column defines class
 y=shuttle(:,10);
 
+% define amount of train and testdata
 train_size = 5000;
 test_size = 500;
 
+% create reandom indices for the retrievel of the datased
 indices = randsample(size(x,1),train_size + test_size);
-% Retrieve 5000 random samples as training data
+% Retrieve random test and traindata from the dataset
 x_train=x(indices(1:train_size),:);
 y_train=y(indices(1:train_size));
 x_test=x(indices((train_size+1):(train_size + test_size)),:);
@@ -19,10 +25,10 @@ classRef = [1 2; 1 3; 1 4];
 
 pred_labels = clsOneVsOne(x_train, y_train, x_test, classRef);
 
-loss01(pred_labels, y_test)
+err_OneVsOne = loss01(pred_labels, y_test)
 
 pred_labels = clsOneVsAll(x_train, y_train, x_test);
 
-loss01(pred_labels, y_test)
+err_OneVsAll = loss01(pred_labels, y_test)
 
-[~, errValue] = evaluateK(x_train, y_train, x_test, y_test)
+[~, err_kNN] = evaluateK(x_train, y_train, x_test, y_test)
