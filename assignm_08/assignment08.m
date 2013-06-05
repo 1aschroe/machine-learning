ce73c6977d1ebe9b42bf7de1f4d85bdfe8044342
Arne Schröder <Arne.Schroeder@gmail.com>
2013-06-05 11:42:15 +0200

    Erzeugen der Daten ausgelagert.

--- assignm_08/assignment08.m
+++ assignm_08/assignment08.m
@@ -1,15 +1,30 @@
+function [] = assignment08()
+
 %% Exercise 3
 
 Sigma=[2 0 0;0 1 0;0 0 4];
 mu=[0,0,0];
-d = 3;
 n = 2000;
 
-S1 = chol(Sigma);
-X = repmat(mu,n,1) + randn(n,d)*S1;
+X = GenerateData(Sigma, mu, n)
 
 plot3(X(:,1), X(:,2), X(:,3), '.')
 
-[v, d] = eig(Sigma)
+v = sqrt(2)/2 * [ 1, 1; -1, 1 ];
+d = [ 3, 0; 0, 1 ];
 
-Sigma*v
+Sigma = v * d * v'
+mu = [0 0];
+
+X = GenerateData(Sigma, mu, 400)
+figure;
+hold on;
+plot(X(:,1), X(:,2), '.');
+end
+
+function [X] = GenerateData(Sigma, mu, n)
+
+S1 = chol(Sigma);
+X = repmat(mu,n,1) + randn(n,size(Sigma,1))*S1;
+
+end
