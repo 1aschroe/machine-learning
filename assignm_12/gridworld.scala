b337d1b77d9ac3cff7524c8d23a1612dfe6510bc
Angel Bakardzhiev <angel.bakardzhiev@informatik.uni-hamburg.de>
2013-07-01 12:53:30 +0200

    Added wall, start and edn point.

--- assignm_12/gridworld.scala
+++ assignm_12/gridworld.scala
@@ -1,11 +1,25 @@
+package hello
+
 object GridWorld {
 
   def main(args: Array[String]) = {
     var grid = generateGridWorld(5,5)
-    grid.foreach((row: Array[Int]) => { row.foreach((element: Int ) => print(element)); println; })
+    grid.foreach((row: Array[Int]) => { row.foreach((element: Int ) => print("\t" + element)); println; })
   }
 
   def generateGridWorld(xSize: Int, ySize: Int): Array[Array[Int]] = {
-    Array.ofDim[Int](xSize,ySize)
+    var result = Array.ofDim[Int](xSize,ySize)
+    
+    result(1)(1) = -1
+    result(1)(2) = -1
+    result(1)(3) = -1
+    result(3)(2) = -1
+    result(4)(2) = -1
+    result(1)(2) = -1
+    
+    result(0)(0) = 1
+    result(4)(4) = 2
+    
+    result
   }
 }
