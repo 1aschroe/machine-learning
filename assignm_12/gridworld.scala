5c4e8ddfc5b1151e7861022bf59e541b45a88f19
Arne Schröder <Arne.Schroeder@gmail.com>
2013-07-03 22:20:49 +0200

    Generierung einer neuen GridWorld erstellt

--- assignm_12/gridworld.scala
+++ assignm_12/gridworld.scala
@@ -15,14 +15,91 @@ object GridWorld {
 
   def main(args: Array[String]) = {
     var qFunction = Array.ofDim[Double](dimensions._1, dimensions._2, 4)
-	for (i <- 1 to 500) {
+    for (i <- 1 to 50) {
       qFunction = runSimulation(grid, qFunction)
-	}
+    }
 
     println("DONE!")
   }
 
   def generateGridWorld(xSize: Int, ySize: Int): Array[Array[Char]] = {
+    var result = Array.fill[Char](xSize, ySize)('X')
+
+    result(0)(0) = 'S'
+
+    makePathInGrid(result, 0, 0)
+    
+    result(lastFreedPosition._1)(lastFreedPosition._2) = 'G'
+
+    result
+  }
+  
+  /*
+   * This might need some simplification
+   */
+  def getPossibleDirections(newGrid: Array[Array[Char]], xPos: Int, yPos: Int): List[Int] = {
+    var possibleDirections: List[Int] = Nil;
+    if ((xPos - 1 >= 0 && newGrid(xPos - 1)(yPos) == 'X') &&
+      (xPos - 2 < 0 || newGrid(xPos - 2)(yPos) == 'X') &&
+      (yPos - 1 < 0 || newGrid(xPos - 1)(yPos - 1) == 'X') &&
+      (yPos + 1 >= newGrid(0).length || newGrid(xPos - 1)(yPos + 1) == 'X')) {
+      possibleDirections = 3 :: possibleDirections
+    }
+    if ((yPos - 1 >= 0 && newGrid(xPos)(yPos - 1) == 'X') &&
+      (yPos - 2 < 0 || newGrid(xPos)(yPos - 2) == 'X') &&
+      (xPos - 1 < 0 || newGrid(xPos - 1)(yPos - 1) == 'X') &&
+      (xPos + 1 >= newGrid.length || newGrid(xPos + 1)(yPos - 1) == 'X')) {
+      possibleDirections = 0 :: possibleDirections
+    }
+    if ((xPos + 1 < newGrid.length && newGrid(xPos + 1)(yPos) == 'X') &&
+      (xPos + 2 >= newGrid.length || newGrid(xPos + 2)(yPos) == 'X') &&
+      (yPos - 1 < 0 || newGrid(xPos + 1)(yPos - 1) == 'X') &&
+      (yPos + 1 >= newGrid(0).length || newGrid(xPos + 1)(yPos + 1) == 'X')) {
+      possibleDirections = 2 :: possibleDirections
+    }
+    if ((yPos + 1 < newGrid(0).length && newGrid(xPos)(yPos + 1) == 'X') &&
+      (yPos + 2 >= newGrid(0).length || newGrid(xPos)(yPos + 2) == 'X') &&
+      (xPos - 1 < 0 || newGrid(xPos - 1)(yPos + 1) == 'X') &&
+      (xPos + 1 >= newGrid.length || newGrid(xPos + 1)(yPos + 1) == 'X')) {
+      possibleDirections = 1 :: possibleDirections
+    }
+    possibleDirections
+  }
+  
+  var lastFreedPosition = (0,0)
+
+  def makePathInGrid(newGrid: Array[Array[Char]], xPos: Int, yPos: Int) {
+    lastFreedPosition = (xPos, yPos)
+
+    var possibleDirections = getPossibleDirections(newGrid, xPos, yPos)
+    while (possibleDirections != Nil) {
+      Random.shuffle(possibleDirections).head match {
+        case 0 => { // nach links
+          newGrid(xPos)(yPos - 1) = ' '
+          makePathInGrid(newGrid, xPos, yPos - 1)
+        }
+        case 1 => { // nach rechts
+          newGrid(xPos)(yPos + 1) = ' '
+          makePathInGrid(newGrid, xPos, yPos + 1)
+        }
+        case 2 => { // nach unten
+          newGrid(xPos + 1)(yPos) = ' '
+          makePathInGrid(newGrid, xPos + 1, yPos)
+        }
+        case 3 => { // nach oben
+          newGrid(xPos - 1)(yPos) = ' '
+          makePathInGrid(newGrid, xPos - 1, yPos)
+        }
+        case _ => {
+          println("BAD!")
+          (-1, -1)
+        }
+      }
+      possibleDirections = getPossibleDirections(newGrid, xPos, yPos)
+    }
+  }
+
+  def generateGridWorldOld(xSize: Int, ySize: Int): Array[Array[Char]] = {
     var result = Array.fill[Char](xSize, ySize)(' ')
 
     result(1)(1) = 'X'
@@ -37,90 +114,94 @@ object GridWorld {
 
     result
   }
-  
-  private def printSituation(positionAgent : (Int, Int)) {
-    val before = grid(positionAgent._1)(positionAgent._2)
-	grid(positionAgent._1)(positionAgent._2) = 'o'
-	println
-    grid.foreach(row => { row.foreach(print); println; })
-	grid(positionAgent._1)(positionAgent._2) = before
-	readLine
+
+  private def printSituation(positionAgent: (Int, Int)) {
+    printSituation(grid, positionAgent)
   }
-  
+
+  private def printSituation(thisGrid: Array[Array[Char]], positionAgent: (Int, Int)) {
+    val before = thisGrid(positionAgent._1)(positionAgent._2)
+    thisGrid(positionAgent._1)(positionAgent._2) = 'o'
+    println
+    thisGrid.foreach(row => { row.foreach(print); println; })
+    thisGrid(positionAgent._1)(positionAgent._2) = before
+    readLine
+  }
+
   val directionDescription = Map(0 -> "left", 1 -> "right", 2 -> "down", 3 -> "up")
-  
-  private def getDirection(positionAgent: (Int, Int), qFunction : Array[Array[Array[Double]]]) : Int = {
+
+  private def getDirection(positionAgent: (Int, Int), qFunction: Array[Array[Array[Double]]]): Int = {
     if (Random.nextDouble > epsilon) {
       getDeterministicDirection(positionAgent, qFunction)
-	} else {
-	  if (verbose) println("Chose epsilon-strategy to walk randomly")
-	  Random.nextInt(4)
-	}
-  }
-  
-  private def getDeterministicDirection(positionAgent: (Int, Int), qFunction : Array[Array[Array[Double]]]) : Int = {
-  	val currentActions = qFunction(positionAgent._1)(positionAgent._2)
-	val maxQ = currentActions.max
-	if (verbose) {
-	  println("==================")
-	  for (i <- 0 until currentActions.length) println(directionDescription(i) + ": " + currentActions(i))
-	  println("==================")
-	}
-	// we want to find all indices, with value maxQ
-	val indicesWithMaxQ = currentActions.zipWithIndex.filter(_._1==maxQ).map(_._2)
-	// then we randomly pick one of these indices
-	Random.shuffle(indicesWithMaxQ.toList).head
-  }
-  private def runSimulation(grid: Array[Array[Char]], qFunction : Array[Array[Array[Double]]]): Array[Array[Array[Double]]] = {
-    var positionAgent = (0, 0)
-	var lastPositionAgent = positionAgent
-	var changedQFunction = qFunction
-	var stepCounter = 0
-	var rewardAccumulated = 0
-	
-    while (grid(positionAgent._1)(positionAgent._2) != 'G') {
-	  if (verbose) printSituation(positionAgent)
-	  val direction = getDirection(positionAgent, qFunction)
-	  if (verbose) println("Want to go " + directionDescription(direction))
-	  val possibleNewPositionAgent : (Int, Int) = 
-      direction match {
-        case 0 => { // nach links
-          (positionAgent._1, positionAgent._2 - 1)
-        }
-        case 1 => { // nach rechts
-          (positionAgent._1, positionAgent._2 + 1)
-        }
-        case 2 => { // nach unten
-          (positionAgent._1 + 1, positionAgent._2)
-        }
-        case 3 => { // nach oben
-          (positionAgent._1 - 1, positionAgent._2)
-        }
-        case _ => {
-          println("BAD!")
-		  (-1, -1)
-        }
-      }
-      val (nextPositionAgent, reward) = evaluateAction(positionAgent, grid, changedQFunction, direction, possibleNewPositionAgent)
-	  val nextDirection = getDirection(nextPositionAgent, changedQFunction)
-	  if (verbose) println("Changing " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + " to " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + " + " + alpha + "*(" + reward + " + " + gamma*changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + " + " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + ") = " + (changedQFunction(positionAgent._1)(positionAgent._2)(direction) + alpha*(reward + gamma*changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + changedQFunction(positionAgent._1)(positionAgent._2)(direction))))
-	  changedQFunction(positionAgent._1)(positionAgent._2)(direction) = changedQFunction(positionAgent._1)(positionAgent._2)(direction) + alpha*(reward + gamma*changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + changedQFunction(positionAgent._1)(positionAgent._2)(direction))
-	  positionAgent = nextPositionAgent
-	  stepCounter = stepCounter + 1
-	  rewardAccumulated = rewardAccumulated + reward
+    } else {
+      if (verbose) println("Chose epsilon-strategy to walk randomly")
+      Random.nextInt(4)
     }
-	println(stepCounter)
-	// println("Took " + stepCounter + " steps")
-	// println("Earned " + rewardAccumulated + " reward")
-	changedQFunction
   }
-  
-  def evaluateAction(positionAgent : (Int, Int), grid : Array[Array[Char]], qFunction : Array[Array[Array[Double]]], direction : Int, newPositionAgent : (Int, Int)) : ((Int, Int), Int) = {
-     if ((0 until grid.length).contains(newPositionAgent._1) && (0 until grid(0).length).contains(newPositionAgent._2) && grid(newPositionAgent._1)(newPositionAgent._2) != 'X') {
-	   val reward = if (grid(newPositionAgent._1)(newPositionAgent._2) == 'G') 100 else -1;
-       (newPositionAgent, reward)
-     } else {
-	   (positionAgent, -1)
-	 }
+
+  private def getDeterministicDirection(positionAgent: (Int, Int), qFunction: Array[Array[Array[Double]]]): Int = {
+    val currentActions = qFunction(positionAgent._1)(positionAgent._2)
+    val maxQ = currentActions.max
+    if (verbose) {
+      println("==================")
+      for (i <- 0 until currentActions.length) println(directionDescription(i) + ": " + currentActions(i))
+      println("==================")
+    }
+    // we want to find all indices, with value maxQ
+    val indicesWithMaxQ = currentActions.zipWithIndex.filter(_._1 == maxQ).map(_._2)
+    // then we randomly pick one of these indices
+    Random.shuffle(indicesWithMaxQ.toList).head
+  }
+  private def runSimulation(grid: Array[Array[Char]], qFunction: Array[Array[Array[Double]]]): Array[Array[Array[Double]]] = {
+    var positionAgent = (0, 0)
+    var lastPositionAgent = positionAgent
+    var changedQFunction = qFunction
+    var stepCounter = 0
+    var rewardAccumulated = 0
+
+    while (grid(positionAgent._1)(positionAgent._2) != 'G') {
+      if (verbose) printSituation(positionAgent)
+      val direction = getDirection(positionAgent, qFunction)
+      if (verbose) println("Want to go " + directionDescription(direction))
+      val possibleNewPositionAgent: (Int, Int) =
+        direction match {
+          case 0 => { // nach links
+            (positionAgent._1, positionAgent._2 - 1)
+          }
+          case 1 => { // nach rechts
+            (positionAgent._1, positionAgent._2 + 1)
+          }
+          case 2 => { // nach unten
+            (positionAgent._1 + 1, positionAgent._2)
+          }
+          case 3 => { // nach oben
+            (positionAgent._1 - 1, positionAgent._2)
+          }
+          case _ => {
+            println("BAD!")
+            (-1, -1)
+          }
+        }
+      val (nextPositionAgent, reward) = evaluateAction(positionAgent, grid, changedQFunction, direction, possibleNewPositionAgent)
+      val nextDirection = getDirection(nextPositionAgent, changedQFunction)
+      if (verbose) println("Changing " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + " to " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + " + " + alpha + "*(" + reward + " + " + gamma * changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + " + " + changedQFunction(positionAgent._1)(positionAgent._2)(direction) + ") = " + (changedQFunction(positionAgent._1)(positionAgent._2)(direction) + alpha * (reward + gamma * changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + changedQFunction(positionAgent._1)(positionAgent._2)(direction))))
+      changedQFunction(positionAgent._1)(positionAgent._2)(direction) = changedQFunction(positionAgent._1)(positionAgent._2)(direction) + alpha * (reward + gamma * changedQFunction(nextPositionAgent._1)(nextPositionAgent._2)(nextDirection) + changedQFunction(positionAgent._1)(positionAgent._2)(direction))
+      positionAgent = nextPositionAgent
+      stepCounter = stepCounter + 1
+      rewardAccumulated = rewardAccumulated + reward
+    }
+    println(stepCounter)
+    // println("Took " + stepCounter + " steps")
+    // println("Earned " + rewardAccumulated + " reward")
+    changedQFunction
+  }
+
+  def evaluateAction(positionAgent: (Int, Int), grid: Array[Array[Char]], qFunction: Array[Array[Array[Double]]], direction: Int, newPositionAgent: (Int, Int)): ((Int, Int), Int) = {
+    if ((0 until grid.length).contains(newPositionAgent._1) && (0 until grid(0).length).contains(newPositionAgent._2) && grid(newPositionAgent._1)(newPositionAgent._2) != 'X') {
+      val reward = if (grid(newPositionAgent._1)(newPositionAgent._2) == 'G') 100 else -1;
+      (newPositionAgent, reward)
+    } else {
+      (positionAgent, -1)
+    }
   }
 }
